99ed8e436e4440d0fb23947eb6bf55e85afce99e
Josh Wright <josh.wright@live.com.au>
2013-05-12 19:13:28 +1000

    Test that data is being shared correctly between authors

--- asgn2/src/asgn2Test/RollingStockTests.java
+++ asgn2/src/asgn2Test/RollingStockTests.java
@@ -1,5 +1,12 @@
 package asgn2Test;
 
+/**
+ * RollingStock Test Class
+ * Test interactions of the RollingStock class for expected outcomes
+ * 
+ * @author Joshua
+ *
+ */
 public class RollingStockTests {
 
 }
