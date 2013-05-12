4dc056c7a80ac71dfdc85a89af960173d1d3dfc3
Josh Wright <josh.wright@live.com.au>
2013-05-12 19:17:31 +1000

    test push

--- asgn2/src/asgn2Test/RollingStockTests.java
+++ asgn2/src/asgn2Test/RollingStockTests.java
@@ -4,9 +4,9 @@ package asgn2Test;
  * RollingStock Test Class
  * Test interactions of the RollingStock class for expected outcomes
  * 
- * @author Joshua
+ * @author JoshuaWright
  *
  */
 public class RollingStockTests {
-
+	static void test(){}
 }
